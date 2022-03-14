3cc4f5985884150b9387e807ca884f1ccbc5950c
Terroface <amir.jab.93@gmail.com>
2022-03-14 12:17:00 +0100

    Added missing col in stg

--- models/staging/stg_candidates.sql
+++ models/staging/stg_candidates.sql
@@ -11,6 +11,7 @@ with cte1 as (
         ,link_url
         -- ,livability
         ,minutes_to_central_station_by_public_transport
+        ,minutes_to_nearest_grocery_store_on_foot
         ,name_of_page
         ,price_gbpk * 1000 as price_gbp
         ,property_id
